989005d78343ab9cd5138bd9a8c6bd69c0c5af19
ivan-susnica-ey <ivan.susnica@rs.ey.com>
2022-07-24 16:34:19 +0200

    button effect

--- Section 11/project - food app/src/components/Layout/HeaderCartButton.js
+++ Section 11/project - food app/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,38 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
   const ctx = useContext(CartContext);
 
+  const [btnAnimation, setBtnAnimation] = useState(false);
+
   const numberOfItems = ctx.items.reduce((a, e) => {
     return a + e.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnAnimation ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (ctx.items.length === 0) {
+      return;
+    }
+    setBtnAnimation(true);
+
+    const timer = setTimeout(() => {
+      setBtnAnimation(false);
+    }, 300);
+    // we need cleanup function in cases we add the items rapidly
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [ctx.items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
