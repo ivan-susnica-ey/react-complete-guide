565bd476c13b4c2ba28a543aa83fa0c995bdfde5
ivan-susnica-ey <ivan.susnica@rs.ey.com>
2022-07-24 17:05:00 +0200

    state change and re-render

--- Section 12/src/App.js
+++ Section 12/src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
 
-import './App.css';
+import "./App.css";
 
 function App() {
+  const [showP, setShowP] = useState(false);
+
+  console.log("APP RUNNING");
+
+  const toggleButtonHandler = () => {
+    setShowP((prevState) => {
+      return !prevState;
+    });
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showP && <p>Look at meee! I'm new!</p>}
+      <Button onClick={toggleButtonHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
