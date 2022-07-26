d79d2902b37418710a6a0018ef1014b538ac01b8
ivan-susnica-ey <ivan.susnica@rs.ey.com>
2022-07-26 13:44:41 +0200

    custom hooks - task manager

---
+++ Section 15/project 2/src/hooks/use-http.js
@@ -0,0 +1,41 @@
+import { useState, useCallback } from "react";
+
+const useHttp = (applyData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(
+    async (requestConfig) => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(requestConfig.url, {
+          method: requestConfig.method ? requestConfig.method : "GET",
+          headers: requestConfig.headers ? requestConfig.headers : {},
+          body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+          //ova logika je da ne mora kod get zahteva da se unose dummy data
+        });
+
+        if (!response.ok) {
+          throw new Error("Request failed!");
+        }
+
+        const data = await response.json();
+
+        applyData(data);
+      } catch (err) {
+        setError(err.message || "Something went wrong!");
+      }
+      setIsLoading(false);
+    },
+    [applyData]
+  );
+
+  return {
+    isLoading: isLoading, // moze i samo isLoading
+    error,
+    sendRequest,
+  };
+};
+
+export default useHttp;
