472c75cd37680ff1918cf196eaf413cb1d902915
ivan-susnica-ey <ivan.susnica@rs.ey.com>
2022-07-31 16:27:49 +0200

    login component implemented

--- Section 18/redux counter/src/components/Counter.js
+++ Section 18/redux counter/src/components/Counter.js
@@ -1,12 +1,12 @@
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux/es/exports";
-import { counterActions } from "../store/reducer";
+import { counterActions } from "../store/counter-slice";
 
 const Counter = () => {
   const dispatch = useDispatch();
 
-  const counter = useSelector((state) => state.counter);
-  const showCounter = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter);
+  const showCounter = useSelector((state) => state.counter.showCounter);
 
   const incrementHandler = () => {
     dispatch(counterActions.increment());
