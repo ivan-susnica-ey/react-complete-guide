baca69aceb540728a47938a6434d9d004fabaa68
ivan-susnica-ey <ivan.susnica@rs.ey.com>
2022-08-09 14:31:33 +0200

    Add navigation and Link(s)

--- Section 20/project2/src/pages/QuoteDetail.js
+++ Section 20/project2/src/pages/QuoteDetail.js
@@ -1,8 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
 
 const QuoteDetail = () => {
   const { quoteId } = useParams();
-  return <h1>Hi from QuoteDetail: {quoteId}</h1>;
+
+  return (
+    <>
+      <h1>Hi from QuoteDetail:</h1>
+      <p>{quoteId}</p>
+      <Route path={`/quotes/${quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </>
+  );
 };
 
 export default QuoteDetail;
