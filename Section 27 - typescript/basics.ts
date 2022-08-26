717bb6f9b4e7a94696924a6eaa197400aad7b739
ivan-susnica-ey <ivan.susnica@rs.ey.com>
2022-08-26 10:31:28 +0200

    Practice functions and function types

--- Section 27 - typescript/basics.ts
+++ Section 27 - typescript/basics.ts
@@ -50,3 +50,13 @@ let people: Person[];
 let course: string | number = 'React - The Complete Guide';
 
 course = 12341;
+
+// Functions & types
+
+function add(a: number, b: number) {
+    return a + b; //inference // infered return value of number
+}
+
+function printOuput(value: any) {
+    console.log(value);
+}
